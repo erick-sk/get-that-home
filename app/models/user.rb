7d2b58c900faed9116ba3de734a4f21438c7f411
humberto delgado <dehumberto@gmail.com>
2020-08-24 10:09:33 -0500

    bcrypt and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :properties
 
     has_many :contacted_properties
-    has_many :properties, :through => :contacted_properties
+    has_many :properties_contacted, :through => :contacted_properties, source: :property
 
     has_many :favorited_properties
-    has_many :properties, :through => :favorited_properties
+    has_many :properties_favorited, :through => :favorited_properties, source: :property
 
     has_many :seen_properties
-    has_many :properties, :through => :seen_properties
+    has_many :properties_seen, :through => :seen_properties, source: :property
 
 end
